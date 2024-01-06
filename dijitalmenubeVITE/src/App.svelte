31a2c9947857f98a9a07a61863d27b3c92dce17d
Hulusi Bumin Yalçın <pybhad@gmail.com>
2024-01-06 05:55:24 +0300

    bugfix

--- dijitalmenubeVITE/src/App.svelte
+++ dijitalmenubeVITE/src/App.svelte
@@ -5,19 +5,28 @@
   const menuObj = storeObj.menu;
 
   import { activeCategory } from "./stores";
+  
+  
+  let justify = 'justify-center p-16 ';
 
-
+  $:{
+    if($activeCategory){
+      justify = 'justify-start p-10';
+    }else{
+      justify = 'justify-center p-16 ';
+    }
+  }
 
 </script>
 
 <body>
   <!--BackgroundImage-->
-  <img src="{bg}" alt="background" class="aspect-auto w-screen h-screen fixed bottom-0 opacity-85">
+  <img src="{bg}" alt="background" class="aspect-auto w-screen h-screen fixed bottom-0 opacity-85 scale-x-105">
 
   <!--Main-->
   <div class="fixed left-0 top-0 h-screen w-screen flex flex-col justify-center">
 
-    <div class="h-full flex flex-col justify-center space-y-5 overflow-auto p-16 w-full">
+    <div class="h-full flex flex-col {justify} space-y-5 overflow-auto w-full">
       {#each Object.entries(menuObj) as category}
         <MenuCategory {category}/>
       {/each}
